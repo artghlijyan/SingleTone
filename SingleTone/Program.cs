97f852ad1f91e42d95d229522bc9f001ee4f68a2
Art <deckuser@hotmail.com>
2020-02-09 19:22:41 +0400

    Add project files.

---
+++ SingleTone/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SingleTone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTone instance1 = SingleTone.GetInstance();
+            SingleTone instance2 = SingleTone.GetInstance();
+
+            Console.WriteLine(Object.ReferenceEquals(instance1, instance2));
+        }
+    }
+}
---
+++ SingleTone/SingleTone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingleTone
+{
+    class SingleTone
+    {
+        private static SingleTone instance;
+
+        protected SingleTone() { }
+
+        public static SingleTone GetInstance()
+        {
+            return instance == null ? instance = new SingleTone() : instance;
+        }
+    }
+}
